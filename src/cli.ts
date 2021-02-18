04451bba3ec1ddeeb430e4eba3b69a0c2ab66a57
gvjacob <gvjacob@outlook.com>
2021-02-18 12:19:24 -0500

    parse args and flags

--- src/cli.ts
+++ src/cli.ts
@@ -1,18 +1,46 @@
+import yargs from 'yargs/yargs';
+import { omit } from 'lodash';
 import { Command, flags } from '@oclif/command';
+import { Props } from './types';
 
 export default class Temples extends Command {
+  TEMPLES_YAML = '.temples.yaml';
+
   static description =
     'Automatically generate files from predefined templates. No more boilerplate.';
 
   static flags = {
     version: flags.version({ char: 'v' }),
+    verbose: flags.boolean({ char: 'V' }),
     help: flags.help({ char: 'h' }),
   };
 
   static args = [{ name: 'generator' }];
 
+  /**
+   * Set to false to manually parse
+   * user's variable arguments
+   *
+   * Parsing unspecified flags is currently not
+   * supported by oclif.
+   */
+  static strict = false;
+
+  /**
+   * Get user props.
+   *
+   * @return {Props}
+   */
+  getUserProps(): Props {
+    const args = yargs(process.argv).argv;
+    return omit(args, '_', '$0') as Props;
+  }
+
   async run() {
-    const { args, flags } = this.parse(Temples);
+    const { args } = this.parse(Temples);
+    const props = this.getUserProps();
+    console.log(args);
+    console.log(props);
     this.log('Welcome to temples');
   }
 }
