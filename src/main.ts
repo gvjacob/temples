77f3e7e870297ea475c8d1e5779e30e6be266fc5
gvjacob <gvjacob@outlook.com>
2021-02-18 19:15:11 -0500

    specify path to temples config

---
+++ src/main.ts
@@ -0,0 +1,84 @@
+import path from 'path';
+import YAML from 'yaml';
+import yargs from 'yargs/yargs';
+import { omit } from 'lodash';
+import { Command, flags } from '@oclif/command';
+
+import { readFile } from './utils';
+import { Props, TemplesConfig } from './types';
+import run from './runner';
+
+const TEMPLES_YAML = '.temples.yaml';
+
+export default class Temples extends Command {
+  static description =
+    'Automatically generate files from predefined templates. No more boilerplate.';
+
+  static flags = {
+    version: flags.version({ char: 'v' }),
+    verbose: flags.boolean({ char: 'V', default: false }),
+    config: flags.string({ char: 'c', default: TEMPLES_YAML }),
+    help: flags.help({ char: 'h' }),
+  };
+
+  static args = [{ name: 'generator' }];
+
+  /**
+   * Set to false to manually parse
+   * user's variable arguments
+   *
+   * Parsing unspecified flags is currently not
+   * supported by oclif.
+   */
+  static strict = false;
+
+  /**
+   * Get user props.
+   *
+   * @return {Props}
+   */
+  getUserProps(): Props {
+    const args = yargs(process.argv).argv;
+    const flags = Object.keys(Temples.flags);
+
+    return omit(args, '_', '$0', ...flags) as Props;
+  }
+
+  /**
+   * Get temples configuration from yaml file.
+   *
+   * @param {string} p - path to config file
+   *
+   * @return {TemplesConfig}
+   */
+  getTemplesConfig(p: string): TemplesConfig {
+    const templesConfigFile = readFile(path.resolve(process.cwd(), p));
+
+    if (!templesConfigFile) {
+      this.error(
+        'Configuration file not found or is empty. Create a .temples.yaml file.',
+      );
+    }
+
+    const templesConfig = YAML.parse(templesConfigFile);
+
+    try {
+      TemplesConfig.check(templesConfig);
+      return templesConfig;
+    } catch (e) {
+      this.error(`Configuration file is invalid.\n\n${e}`);
+    }
+  }
+
+  async run() {
+    const { args, flags } = this.parse(Temples);
+    const { generator } = args;
+
+    const props = this.getUserProps();
+    const temples = this.getTemplesConfig(flags.config);
+
+    if (generator) {
+      run(generator, props, temples, flags.verbose);
+    }
+  }
+}
